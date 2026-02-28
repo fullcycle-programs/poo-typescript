0b4651b230141d0fe0246506e38bd15bc4d01e08
progsamdev <samuel.msbr@gmail.com>
2026-02-28 12:58:52 -0300

    fixup! refactor: update User class to encapsulate properties and add getter/setter methods

--- src/User.ts
+++ src/User.ts
@@ -1,32 +1,19 @@
 export default class User {
     private name: string;
     private email: string;
-    private age: number;
+    private age: number = 0;
     private password: string;
     private isAdmin: boolean;
 
     constructor(name: string, email: string, age: number, password: string, isAdmin: boolean) {
         this.name = name;
-        this.email = email;
-        this.age = age;
+        this.email = email; 
+        this.setAge(age);
         this.password = password;
         this.isAdmin = isAdmin;
-
         console.log(`User ${this.name} created successfully`);
     }
-
-    public getName(): string {
-        return this.name;
-    }
-
-    public getEmail(): string {
-        return this.email;
-    }
-    
-    public getAge(): number {
-        return this.age;
-    }
-
+ 
     public setAge(age: number): void {
         if (age < 0 || age > 120) {
             throw new Error("Age must be between 0 and 120");
